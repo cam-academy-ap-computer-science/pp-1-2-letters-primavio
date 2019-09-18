24e5d818fed2a3e31970500bd8efe77ce634d116
Violetta <Violetta@192.168.0.5>
2019-09-18 08:29:47 -0700

    try attempt

--- src/Letters.java
+++ src/Letters.java
@@ -2,8 +2,12 @@
 public class Letters {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		friend1();
+		friend2();
+		friend3();
 	}
 
+	public static void friend1() {
+		paragraphOne("Dear friend, how are you?");
+	}
 }
