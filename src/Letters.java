2f8285c4755181f4c7c682d3c8fcb4ce47439664
Violetta <Violetta@192.168.0.5>
2019-09-18 08:43:05 -0700

    hopefully an ok letter that I actually would never send to anyone

--- src/Letters.java
+++ src/Letters.java
@@ -8,6 +8,26 @@ public class Letters {
 	}
 
 	public static void friend1() {
-		paragraphOne("Dear friend, how are you?");
+		System.out.println("Dear friend, how are you?");
+		System.out.println("I am great! School has started again,");
+		System.out.println("And instead of sleep I've been drinking unhealthy amounts of coffee.");
+		System.out.println("I really wish I would go to sleep earlier, but I cannot.");
+	}
+	
+	public static void friend2() {
+		friend1();
+		System.out.println("");
+		System.out.println("Now on a different topic, my friends all tell me to get a new job,");
+		System.out.println("But I need to earn money!");
+		System.out.println("I wish someone would give me money, but I just cannot fathom as to who would!");
+	}
+	
+	public static void friend3() {
+		friend1();
+		System.out.println("");
+		System.out.println("But to be frank, I am enjoying school.");
+		System.out.println("Junior year really did destroy everything in me,");
+		System.out.println("And at this point, I cannot do anything that will be worse.");
+		System.out.println("I regret doubting anyone who has said it was the worst year.");
 	}
 }
